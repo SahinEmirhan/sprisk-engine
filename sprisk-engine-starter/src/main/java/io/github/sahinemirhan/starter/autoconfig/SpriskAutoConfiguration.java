f11f178fbb4c813334ac0fd32a2702f354dd9ec0
Emirhan Şahin <sahnemirhan@gmail.com>
2025-10-30 05:26:34 +0300

    release v0.0.2

--- sprisk-engine-starter/src/main/java/io/github/sahinemirhan/starter/autoconfig/SpriskAutoConfiguration.java
+++ sprisk-engine-starter/src/main/java/io/github/sahinemirhan/starter/autoconfig/SpriskAutoConfiguration.java
@@ -40,6 +40,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationEventPublisher;
